89fa61cb82c0c47ae4b492127481ebc6dc304af0
mattiaSenni <mattia.senni.stud@ispascalcomandini.it>
2021-05-05 11:48:11 +0200

    first commit

---
+++ TestTCP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace TestTCP
+{
+    class Program
+    {
+        static int port = 2021; 
+        static void Main(string[] args)
+        {
+            bool tuaMamma = true;
+            while(tuaMamma)
+            {
+                //Console.Clear();
+                Console.WriteLine("insersci il messaggio : [ip] [porta] [messaggio]");
+                string[] mex = Console.ReadLine().Split(' ');
+                string ip = mex[0];
+                int porta = int.Parse(mex[1]);
+                string message = mex[2] + " " + mex[3];
+                byte[] data = Encoding.ASCII.GetBytes(message);
+
+                Socket s = new Socket(SocketType.Stream, ProtocolType.IP);
+                s.Connect(IPAddress.Parse(ip), porta);
+                //s.Bind(new IPEndPoint(IPAddress.Any, porta));
+
+                for (int i = 0; i < data.Length;)
+                    i += s.Send(data, i, data.Length - i, SocketFlags.None);
+            }
+        }
+    }
+}
